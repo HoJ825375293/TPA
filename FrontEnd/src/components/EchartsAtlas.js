e35bdd82a2a8a1218c11e6be42aacd8416490d4e
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2019-11-18 19:35:06 +0800

    -

--- FrontEnd/src/components/EchartsAtlas.js
+++ FrontEnd/src/components/EchartsAtlas.js
@@ -1,89 +1,16 @@
-import React, { Component, Fragment } from 'react';
-import { Button, message, Avatar } from 'antd';
-import { Link } from 'react-router-dom';
-import store from "../store";
+import React, { Component } from 'react';
+
+var echarts = require('echarts');
 
 class EchartsAtlas extends Component {
-    myChart.showLoading();
-    $.get(ROOT_PATH + 'data/asset/data/les-miserables.gexf', function (xml) {
-    myChart.hideLoading();
-
-    var graph = echarts.dataTool.gexf.parse(xml);
-    var categories = [];
-    for (var i = 0; i < 9; i++) {
-        categories[i] = {
-            name: '类目' + i
-        };
-    }
-    graph.nodes.forEach(function (node) {
-        node.itemStyle = null;
-        node.value = node.symbolSize;
-        node.symbolSize /= 1.5;
-        node.label = {
-            normal: {
-                show: node.symbolSize > 30
-            }
-        };
-        node.category = node.attributes.modularity_class;
-    });
-    option = {
-        title: {
-            text: 'Les Miserables',
-            subtext: 'Default layout',
-            top: 'bottom',
-            left: 'right'
-        },
-        tooltip: {},
-        legend: [{
-            // selectedMode: 'single',
-            data: categories.map(function (a) {
-                return a.name;
-            })
-        }],
-        animationDuration: 1500,
-        animationEasingUpdate: 'quinticInOut',
-        series : [
-            {
-                name: 'Les Miserables',
-                type: 'graph',
-                layout: 'none',
-                data: graph.nodes,
-                links: graph.links,
-                categories: categories,
-                roam: true,
-                focusNodeAdjacency: true,
-                itemStyle: {
-                    normal: {
-                        borderColor: '#fff',
-                        borderWidth: 1,
-                        shadowBlur: 10,
-                        shadowColor: 'rgba(0, 0, 0, 0.3)'
-                    }
-                },
-                label: {
-                    position: 'right',
-                    formatter: '{b}'
-                },
-                lineStyle: {
-                    color: 'source',
-                    curveness: 0.3
-                },
-                emphasis: {
-                    lineStyle: {
-                        width: 10
-                    }
-                }
-            }
-        ]
-    };
-
-    myChart.setOption(option);
-}, 'xml');
-
+    Draw(){
+        document.getElementById("Atlas").removeAttribute("_echarts_instance_")
+        var myChart = echarts.init(document.getElementById("Atlas"));}
     render(){
         return (
             <div>
-                
+            <div>{this.Draw()} </div> 
+            <div id = "Atlas" style = {{width:500, height:400}}> </div>
             </div>
         );
     }
