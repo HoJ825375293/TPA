a545bc263cf7a16200c2688a9fc71edbeeb2f075
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2019-11-25 19:11:35 +0800

    -

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -4,14 +4,14 @@ import HomePage from './pages/HomePage';
 import * as serviceWorker from './components/serviceWorker';
 import 'antd/dist/antd.css'
 import { HashRouter as Router, Route } from 'react-router-dom';
-import LogPage from './pages/LogPage';
-import RegisterPage from './pages/RegisterPage';
+// import LogPage from './pages/LogPage';
+// import RegisterPage from './pages/RegisterPage';
 
 ReactDOM.render(
         <Router>
                 <Route path="/" exact component={HomePage} />
-                <Route path="/login" component={LogPage} />
-                <Route path="/register" component={RegisterPage} />
+                {/* <Route path="/login" component={LogPage} /> */}
+                {/* <Route path="/register" component={RegisterPage} /> */}
         </Router>
 , document.getElementById('root'));
 
